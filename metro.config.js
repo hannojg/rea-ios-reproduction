2c516c03196ae603f7ecf92b9c381d83b914cc06
Hanno J. Gödecke <die.drei99@yahoo.de>
2021-10-05 10:55:59 +0200

    upgrading to RN 0.65.1 and enabling hermes (ios still working)

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const originalTransformer = defaultConfig.transformer.getTransformOptions;
+defaultConfig.transformer.getTransformOptions = async () => {
+  let transformConfig = {};
+  if (typeof originalTransformer === "function") {
+    transformConfig = (await originalTransformer()) || {};
+  }
+  transformConfig.transform = {
+    ...(transformConfig.transform || {}),
+    inlineRequires: true,
+  }
+
+  return transformConfig;
+}
+
+module.exports = defaultConfig;
